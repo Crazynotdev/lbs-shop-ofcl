006cac6e34672e91f76757f0680182d1ee44af94
ZynXx <techigwe@gmail.com>
2026-05-24 08:19:54 +0100

    Update Tailwind CSS content paths for configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './libs/**/*.{js,ts,jsx,tsx,mdx}',
+    './store/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
